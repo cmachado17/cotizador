ae7731246b3611bdd1a3ebf15b69d3cfdc8c2365
cmachado17 <machado.cristian1217@gmail.com>
2021-06-03 15:40:52 -0300

    state, values desde formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -6,11 +6,20 @@ const Formulario = () => {
 
   const [cotizacion, setCotizacion] = useState({ ...datosIniciales });
 
+  const {marca, año, origen, plan} = cotizacion;
+
+  //obtener datos del formulario
   const cotizar = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
     setCotizacion({ ...cotizacion, [name]: value });
   };
+
+  const realizarCotizacion = (e) => {
+      e.preventDefault();
+
+      setCotizacion(datosIniciales);
+  }
 
   //styles components
   const Campo = styled.div`
@@ -27,7 +36,8 @@ const Formulario = () => {
     <form action="#" className="bg-dark p-5 text-light">
       <Campo className="mb-3">
         <Label>Marca</Label>
-        <select name="marca" onChange={cotizar}>
+        <select name="marca" value={marca} onChange={cotizar}>
+        <option value="" defaultValue>Seleccione una marca</option>
           <option value="Ford">Ford</option>
           <option value="Chevrolet">Chevrolet</option>
           <option value="Fiat">Fiat</option>
@@ -36,7 +46,8 @@ const Formulario = () => {
       </Campo>
       <Campo className="mb-3">
         <Label>Año</Label>
-        <select name="año" onChange={cotizar}>
+        <select name="año" value={año} onChange={cotizar}>
+        <option value="" defaultValue>Seleccione un año</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
           <option value="2019">2019</option>
@@ -49,6 +60,7 @@ const Formulario = () => {
           type="radio"
           name="origen"
           value="Americano"
+          checked ={origen === "Americano"}
           onChange={cotizar}
         />
         Americano
@@ -56,23 +68,25 @@ const Formulario = () => {
           type="radio"
           name="origen"
           value="Europeo"
+          checked ={origen === "Europeo"}
           onChange={cotizar}
         />
         Europeo
       </Campo>
       <Campo className="mb-3">
         <Label>Plan</Label>
-        <InputRadio type="radio" name="plan" value="Full" onChange={cotizar} />
+        <InputRadio type="radio" name="plan" value="Full" checked ={plan === "Full"} onChange={cotizar} />
         Full
         <InputRadio
           type="radio"
           name="plan"
           value="Economico"
+          checked = {plan === "Economico"}
           onChange={cotizar}
         />
         Economico
       </Campo>
-      <button className="btn btn-block btn-success">Enviar</button>
+      <button className="btn btn-block btn-success" onClick={realizarCotizacion}>Enviar</button>
     </form>
   );
 };
